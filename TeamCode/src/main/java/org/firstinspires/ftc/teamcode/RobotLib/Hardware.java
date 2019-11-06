db38b39b10e90bcd8e0603994e07be304bb2ea19
Retinova <rguter8@gmail.com>
2019-11-06 14:56:35 -0500

    new odometry branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLib/Hardware.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.RobotLib;
-
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public interface Hardware {
-    void init(HardwareMap hwMap);
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLib/Robot.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.RobotLib;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public abstract class Robot extends LinearOpMode implements Hardware{
-    public abstract void init(HardwareMap hwMap);
-    public abstract void drive();
-}
