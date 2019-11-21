80871ce2073fa392f6a0e97395e4a58792f28370
Retinova <rguter8@gmail.com>
2019-11-21 17:16:44 -0500

    uhhh did the device filter thingy for usb, did the big funny buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometryinput/MouseTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometryinput/MouseTest1.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.odometryinput;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 
+@TeleOp(name="mouse test", group="mouse")
 public class MouseTest1 extends LinearOpMode {
 
     I2cDeviceSynch mouse;
@@ -11,11 +14,16 @@ public class MouseTest1 extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         mouse = hardwareMap.i2cDeviceSynch.get("mouse");
 
-        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(1, 6, I2cDeviceSynch.ReadMode.REPEAT);
+        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(0, 2, I2cDeviceSynch.ReadMode.REPEAT);
 
         mouse.setReadWindow(readWindow);
 
+        mouse.setI2cAddress(new I2cAddr(0x00));
+
         mouse.engage();
+
+        telemetry.addData("Mouse:", "Initialized");
+        telemetry.update();
 
         waitForStart();
 
@@ -23,7 +31,8 @@ public class MouseTest1 extends LinearOpMode {
             telemetry.addData("Byte 0: ", mouse.read8(0));
             telemetry.addData("Byte 1: ", mouse.read8(1));
             telemetry.addData("Byte 2: ", mouse.read8(2));
-            telemetry.addData("Byte 3: ", mouse.read8(3));
+//            telemetry.addData("Byte 3: ", mouse.read8(3));
+            telemetry.update();
         }
     }
 }
