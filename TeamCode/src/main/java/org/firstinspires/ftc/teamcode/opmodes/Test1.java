ba2329dc3e9a06d115fe09671ebaedf7dc9bf527
Retinova <rguter8@gmail.com>
2019-11-29 10:47:43 -0500

    Revert "finalizing movement for 11/30/19 qualifier"
    This reverts commit 9f02e0b1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Test1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Test1.java
@@ -1,46 +1,21 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.supers.Robot;
 
-@TeleOp(name="test of robot and also code modularization", group="teleop")
+import org.firstinspires.ftc.teamcode.supers.Auto;
+import org.firstinspires.ftc.teamcode.supers.Globals;
+import org.firstinspires.ftc.teamcode.supers.Hardware;
+
 public class Test1 extends LinearOpMode {
-    Robot robot;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        Globals.test1 = this;
 
-        robot = new Robot(this);
-
-        robot.init();
-
-        robot.win();
+        Hardware.init(hardwareMap);
 
         waitForStart();
 
-        while(opModeIsActive()) {
-            if (gamepad1.a) {
-                robot.tele.lf.setPower(1.0);
-                robot.tele.lb.setPower(1.0);
-                robot.tele.rf.setPower(1.0);
-                robot.tele.rb.setPower(1.0);
-            }
-            if (gamepad1.b) {
-                robot.tele.lf.setPower(0.0);
-                robot.tele.lb.setPower(0.0);
-                robot.tele.rf.setPower(0.0);
-                robot.tele.rb.setPower(0.0);
-            }
-
-            if (gamepad1.right_bumper) robot.tele.servo.setPosition(0.2);
-            if (gamepad1.left_bumper) robot.tele.servo.setPosition(0.8);
-
-            if (gamepad1.y) robot.tele.crServo.setPower(gamepad1.left_stick_y);
-            else robot.tele.crServo.setPower(0);
-
-            telemetry.addData("Servo power: ", "%.2f", robot.tele.crServo.getPower());
-            telemetry.update();
-        }
+        Auto.drive(5);
     }
 }
